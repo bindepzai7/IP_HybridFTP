2a5cfe0c0b123fc93178b1e6a77ccf61a7ed3ac0
bindepzai7 <doanductuan297@gmail.com>
2026-06-22 18:20:34 +0700

    add session states

--- hybrid_ftp/server/session.py
+++ hybrid_ftp/server/session.py
@@ -1,3 +1,20 @@
+import uuid
+
 class Session:
-    def __init__():
-        pass
+    def __init__(self, authenticator):
+        self.id = str(uuid.uuid4())
+
+        self.authenticator = authenticator
+
+        self.username = None
+        self.logged_in = False
+        
+    def set_user(self, username):
+        self.username = username
+        
+    def login(self):
+        self.logged_in = True
+        
+    def logout(self):
+        self.username = None
+        self.logged_in = False
